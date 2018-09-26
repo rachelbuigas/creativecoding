8aef28bb667572abe8bb397334d190d14ac487d2
rachelbuigas <31712935+rachelbuigas@users.noreply.github.com>
2018-09-26 13:40:04 -0400

    Homework 9-27

---
+++ p5/9_27_Homework/9_27_Homework.js
@@ -0,0 +1,20 @@
+function setup () {
+	//myVariable is now an int
+	myVariable = 5;
+	createCanvas(600,500);
+}
+
+function draw () {
+	if (mouseX==500){
+		background(0)
+	}
+	else if(mouseX>(width/2)){
+		background(255,0,0);
+	}
+	else if(mouseX<=(width/2) && mouseY>(height/3)){
+		background(0,255,0);
+	}
+	else {
+		background(0,0,255);
+	}
+}
